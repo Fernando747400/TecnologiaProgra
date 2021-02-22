4de078765bd39accd74313147c16f36b3ba5bf08
Fernando747 <fernandojcr5@gmail.com>
2021-02-21 21:41:44 -0600

    Seleccion

--- Calculadora/Assets/_Codigo/Calculadora.cs
+++ Calculadora/Assets/_Codigo/Calculadora.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class Calculadora : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ToggleGroup TG;
+    
+
     void Start()
     {
+        if(TG == null) TG = GetComponent<ToggleGroup>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Seleccion();
     }
+
+    public string Seleccion()
+    {
+        string selec;
+        Toggle selectedToggle = TG.ActiveToggles().FirstOrDefault();
+       return selec = selectedToggle.name;
+    }
+
+
 }
